d98ed6e90279b50ef941dffe0f6dd68109abd332
Alalar32 <dotseelijah0@gmail.com>
2025-11-11 11:01:59 +0000

    add the home page content and syling but not done just left with small touches

--- app_public/static/public/js/main.js
+++ app_public/static/public/js/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".navbar");
-  if (window.scrollY < 50) {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
