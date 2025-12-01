404db3a3ec4b4c50e4fdd8dd71ed26cdc33b4153
Sebastian Akaxoxo <sebsion8@gmail.com>
2025-12-01 23:54:46 +0000

    Add forgot password functionality and update links in login and register pages

--- app_accounts/urls.py
+++ app_accounts/urls.py
@@ -6,4 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    
 ]
